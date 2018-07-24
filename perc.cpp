0abe277bbf9b7c9731441015317ec822c5338a58
jrontynen <joel.rontynen@gmail.com>
2018-07-24 13:27:37 -0400

    Added wrapping detection in perc.cpp.

--- perc.cpp
+++ perc.cpp
@@ -2,7 +2,7 @@
 #include <fstream>
 using namespace std;
 
-const int L {5};
+const int L {9};
 const int N {L*L};
 const int EMPTY {-(N+1)};
 
@@ -12,8 +12,8 @@ const int EMPTY {-(N+1)};
 int ptr[N]; // Array of pointers
 int nn[N][4]; // Nearest neighbors
 int order[N]; // Occupation order
-int dx[N];
-int dy[N];
+int dx[N]; // vertical distance of the path from the site to its root
+int dy[N]; // horizontal distance of the path from the site to its root
 
 
 void boundaries() {
@@ -46,8 +46,8 @@ void permutation() {
   }
 	
   // Permute:
+  srand (time(NULL));
   for (i=0; i<N; i++) {
-    //j = i+(N-i)*RNGLONG;
     j = i+rand()%(N-i); //random index in the range (i+1)...(N-1)
 		
     //Switch the order of i and j:
@@ -59,19 +59,15 @@ void permutation() {
 
 
 int findroot(int i) {
-  //Finds the root site of the site with an index i. Uses path compression.
+  //Finds the root site of the site with an index i.
+  //Uses path compression and keeps track of the length of the path.
 	
-  //The site with an index i is its own root, i.e. does not point anywhere:
-  if (ptr[i]<0) { 
-    return [i,0,0];
+  if (ptr[i]<0) { // the site i does not point anywhere (root site)
+    return i;
   }
-  
-  //Else find the root of the site where the site i points to, and make the site i point to the root:
-  [r,dx,dy] = findroot(ptr[i]);
-  ptr[i] = r;
-  dx[i] = dx + ;
-  dy[i] = dy;
-  return [r,dx,dy];
+  dx[i] += dx[ptr[i]]; // add the horizontal distance of the pointer to its own pointer
+  dy[i] += dy[ptr[i]]; 
+  return ptr[i] = findroot(ptr[i]); // go through the tree until the root site is found
 }
 
 /*
@@ -90,11 +86,77 @@ int findroot(int i) {
 */
 
 
+void print_init(ofstream & file) {
+  
+  file << "L=" << L << ", N=" << N << ", EMPTY=" << EMPTY << endl;
+  file << endl << "Lattice indices: " << endl;
+  
+  for (int n=0; n<N; n++) {
+    file << n << " ";
+    if (n<10) {
+      file << " ";
+    }
+    if (n%L == L-1) {
+      file << endl;
+    }
+  }
+  file << endl << "----------------------------------------------" << endl;
+}
+
+void print_lattice(int n, ofstream & file) {
+  
+  int i;
+  
+  file << "n = " << n << ":" << endl;
+    
+  for (i=0; i<N; i++) {
+    if (ptr[i]!=EMPTY) {
+      file << "x ";
+    }
+    else {
+      file << ". ";
+    }
+    if (i%L == L-1) {
+      file << endl;
+    }
+  }
+
+  file << "ptr:" << endl;
+  for (i=0; i<N; i++) {
+    if (-9<=ptr[i] && ptr[i]<=-1) file << " ";
+    if (0<=ptr[i] && ptr[i]<=9) file << "  ";
+    if (ptr[i]>=10) file << " ";
+    file << ptr[i] << " ";
+    if (i%L == L-1) file << endl;
+  }
+
+  file << "dx:" << endl;
+  for (i=0; i<N; i++) {
+    if (dx[i]>-1) file << " ";
+    file << dx[i] << " ";
+    if (i%L == L-1) file << endl;
+  }
+
+  file << "dy:" << endl;
+  for (i=0; i<N; i++) {
+    if (dy[i]>-1) file << " ";
+    file << dy[i] << " ";
+    if (i%L == L-1) file << endl;
+  }
+
+  file << endl << "----------------------------------------------" << endl;
+}
+  
+
+
+
 void percolate() {
 	
-  ofstream f_big;
+  ofstream f_big, f_mov;
   f_big.open("biggest.txt");
-  f_big << L << endl;
+  f_big << "L=" << L << endl << "n big" << endl;
+  f_mov.open("movie.txt");
+  print_init(f_mov);
   
   int i,j;
   int s1,s2;
@@ -111,57 +173,55 @@ void percolate() {
 	
   //Percolate:
   for (i=0; i<N; i++) {
-    r1 = s1 = order[i]; // choose an empty site to be occupied (s1) which is its own root (r1)
+    r1 = s1 = order[i]; // choose an empty site to be occupied (s1) and set r1 = s1 
     ptr[s1] = -1; // the occupied site points nowhere and has size 1.
     for (j=0; j<4; j++) { // loop over the nearest neighbours
       s2 = nn[s1][j]; // index of a neighbor (s2)
       if (ptr[s2]!=EMPTY) { // if the neighbor is occupied
-	[r2,dx2,dy2] = findroot(s2); // find the root of an occupied neighbor (r2)
+	r2 = findroot(s2); // find the root of an occupied neighbor (r2)	  
 	if (r2!=r1) { // if the occupied neighbor belongs to a different cluster (roots r1 and r2 are different)
 	  if (ptr[r1]>ptr[r2]) { // if the neighbor belongs to a larger cluster
 	    ptr[r2] += ptr[r1]; // add the size of the current cluster to the neighboring cluster
 	    ptr[r1] = r2; // the occupied site points to the root of the neighboring larger cluster
-	    dx[r1] = dx2 + r1%L - ;
-	    dy[r1] = dy[r2] - dy[r1];	    
-	    r1 = r2; // r1 changes to the root of the larger cluster
+	    dx[r1] = -dx[s1] + (j/2-1)*(2*j-1) + dx[s2]; //update the distace from r1 to its new root r2
+	    dy[r1] = -dy[s1] + (j/2)*(2*(j/3)-1) + dy[s2];
+	    r1 = r2; // r2 is the new root of the combined cluster
 	  } 
-	  else { // if the neighbor belongs to a smaller cluster
+	  else { // if the neighbor belongs to a smaller or equal size cluster
 	    ptr[r1] += ptr[r2]; // add the size of the neighboring cluster to the current cluster
-	    ptr[r2] = r1; // the root of the neighboring cluster points to the occupied site
-	    dx[s2] = dx2 + (r2%L - r1%L);
-	    dy[s2] = dy2 + (r2/L - r1/L);
+	    ptr[r2] = r1; // the root of the neighboring cluster points to the root of the current cluster
+	    dx[r2] = -dx[s2] - (j/2-1)*(2*j-1) + dx[s1]; //update the distace from r2 to its new root r1
+	    dy[r2] = -dy[s2] - (j/2)*(2*(j/3)-1) + dy[s1];
 	    // r1 remains unchaged
 	  }
 	  // Keep track of the size of the biggest cluster:
 	  if (-ptr[r1]>big) { // if the size of the joined cluster is larger than the biggest cluster
 	    big = -ptr[r1];
-	  }
+	    f_big << i+1 << " " << big << endl;
+	  } 
+	}
+	else { //the neighbor belongs to the same cluster
 	  //Check for percolation:
-	  
+	  if (abs(dx[s1]-dx[s2]) + abs(dy[s1]-dy[s2]) != 1 && perc_ind==0) {
+	    f_mov << endl << "PERCOLATION!" << endl;
+	    f_mov << "The difference of the distances of sites " << s1 <<" and "<< s2;
+	    f_mov << " to their shared root site is not equal to unity."<<endl<<endl;
+	    perc_ind=1; // do not repeat the percolation message
+	  }
 	}
       }
     }
+    print_lattice(i+1,f_mov);
   }
   f_big.close();
+  f_mov.close();
 }
 
 
 
-int main(){
-  boundaries();
-  permutation();
-	
-  int n {0};
-  cout << n << endl;
-  for (int i {0}; i<4; i++) {
-    cout << nn[n][i] << " ";
-  }
-  cout << endl;
-	
-  for (int i {0}; i<N; i++) {
-    cout << order[i] << " ";
-  }
-  cout << endl;
-	
+int main() {
+    boundaries();
+    permutation();
+    percolate();
 	
 }
